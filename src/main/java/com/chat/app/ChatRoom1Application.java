3028792b1ccc1b5213c9b70cf532bd9551dbd40e
AMAN-KUMAR-O7 <amankumarmarch2000@gmail.com>
2022-06-09 20:46:30 +0530

    first commit

---
+++ src/main/java/com/chat/app/ChatRoom1Application.java
@@ -0,0 +1,13 @@
+package com.chat.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRoom1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatRoom1Application.class, args);
+	}
+
+}
